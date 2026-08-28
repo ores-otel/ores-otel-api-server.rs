dead4cebf31c34b84e0cafb356d640954cf87648
alex-mills <alexander.d.mills@gmail.com>
2026-08-27 19:02:53 -0500

    Load API config from src/env overlay; disable cwd .env for the server.

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,5 @@
 #![forbid(unsafe_code)]
 
-#[path = "../generated/rust/env.rs"]
-mod env;
-
-
 use crate::env_map::{value, EnvMap};
 
 #[derive(Clone, Debug)]
@@ -16,15 +12,16 @@ pub struct ApiConfig {
 impl ApiConfig {
     pub fn from_env_map(env: &EnvMap) -> Self {
         Self {
-            bind: value(env, env::BIND)
+            bind: value(env, crate::env::BIND)
                 .unwrap_or("127.0.0.1:8080")
                 .to_owned(),
-            tcp_bind: value(env, env::TCP_BIND).map(str::to_owned),
-            nats_url: value(env, env::NATS_URL).map(str::to_owned),
+            tcp_bind: value(env, crate::env::TCP_BIND).map(str::to_owned),
+            nats_url: value(env, crate::env::NATS_URL).map(str::to_owned),
         }
     }
 
     pub fn from_env() -> Self {
-        Self::from_env_map(&std::env::vars().collect())
+        let env = crate::env::load().unwrap_or_else(|err| panic!("{err}"));
+        Self::from_env_map(&env)
     }
 }
